bbc8fbeec60a446f6e3f35eb90d3c04229030c96
kalucky0 <kalucki23@gmail.com>
2025-12-07 18:27:22 +0100

    Remove Duotone, Fill, Light, and Thin icons

--- lib/src/phosphor_icon.dart
+++ lib/src/phosphor_icon.dart
@@ -1,7 +1,6 @@
 library phosphor_flutter;
 
 import 'package:flutter/material.dart';
-import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class PhosphorIcon extends Icon {
   const PhosphorIcon(
@@ -19,49 +18,24 @@ class PhosphorIcon extends Icon {
     this.duotoneSecondaryOpacity = 0.20,
     this.duotoneSecondaryColor,
   }) : super(
-          icon,
-          color: color,
-          fill: fill,
-          grade: grade,
-          key: key,
-          opticalSize: opticalSize,
-          semanticLabel: semanticLabel,
-          shadows: shadows,
-          size: size,
-          textDirection: textDirection,
-          weight: weight,
-        );
+         icon,
+         color: color,
+         fill: fill,
+         grade: grade,
+         key: key,
+         opticalSize: opticalSize,
+         semanticLabel: semanticLabel,
+         shadows: shadows,
+         size: size,
+         textDirection: textDirection,
+         weight: weight,
+       );
 
   final double duotoneSecondaryOpacity;
   final Color? duotoneSecondaryColor;
 
   @override
   Widget build(BuildContext context) {
-    if (icon is PhosphorDuotoneIconData) {
-      final duotoneIcon = icon as PhosphorDuotoneIconData;
-      return Stack(
-        alignment: Alignment.center,
-        children: [
-          Opacity(
-            opacity: duotoneSecondaryOpacity,
-            child: Icon(
-              duotoneIcon.secondary,
-              key: key,
-              size: size,
-              fill: fill,
-              weight: weight,
-              grade: grade,
-              opticalSize: opticalSize,
-              color: duotoneSecondaryColor ?? color,
-              shadows: shadows,
-              semanticLabel: semanticLabel,
-              textDirection: textDirection,
-            ),
-          ),
-          super.build(context),
-        ],
-      );
-    }
     return super.build(context);
   }
 }
--- lib/src/phosphor_icon_data.dart
+++ lib/src/phosphor_icon_data.dart
@@ -4,22 +4,15 @@ import 'package:flutter/widgets.dart';
 
 class PhosphorIconData extends IconData {
   const PhosphorIconData(int codePoint, String style)
-      : super(
-          codePoint,
-          fontFamily: 'Phosphor$style',
-          fontPackage: 'phosphor_flutter',
-          matchTextDirection: true,
-        );
+    : super(
+        codePoint,
+        fontFamily: 'Phosphor$style',
+        fontPackage: 'phosphor_flutter',
+        matchTextDirection: true,
+      );
 }
 
 class PhosphorFlatIconData extends PhosphorIconData {
   const PhosphorFlatIconData(int codePoint, String style)
-      : super(codePoint, style);
-}
-
-class PhosphorDuotoneIconData extends PhosphorIconData {
-  const PhosphorDuotoneIconData(int codePoint, this.secondary)
-      : super(codePoint, 'Duotone');
-
-  final PhosphorIconData secondary;
+    : super(codePoint, style);
 }
